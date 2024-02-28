8675c68261896b3c96b9499bfa1b84fa7b532d27
Samuele Daminato <samuele.daminato@usi.ch>
2024-02-28 22:41:25 +0100

    created skeleton of folders

---
+++ 0-OnePageSummary/JavaSintesi.java
@@ -0,0 +1,10 @@
+// open -a "visual studio code" lab-02-samdamination
+
+public class proveJAVA{
+    public static void main(String[] args){
+        System.out.println("prova");
+        System.out.println(double(20/32));
+        System.out.println(0.625);
+
+    }
+}
